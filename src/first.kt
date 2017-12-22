e7dce6cfb6512fa01749e3072db1bf9770a5c09f
mohammadrezaDahaghin <eyoohan@yahoo.com>
2017-12-22 17:12:23 +0330

    first kotlin

---
+++ src/first.kt
@@ -0,0 +1,18 @@
+
+fun main (args:Array<String>){
+    var arr=ArrayList<Int>()
+    arr.add(0,100)
+//    arr.add(2,200)
+//    arr.add(1,20)
+    arr.add(50)
+    arr.add(100)
+    arr.add(50)
+    arr.add(200)
+    print(arr.clone())
+
+//    for (x in 0..arr.size-1){
+//    println(arr[x])
+//
+//    }
+
+}
